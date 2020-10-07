e1a1930a82bbcd662114238f0652ae585336fc35
Alan Almeida <alan241015@gmail.com>
2020-10-07 10:33:14 +0530

    Add Support To Uppercase on Seedwords

--- Word List Generator/wordlistgen.py
+++ Word List Generator/wordlistgen.py
@@ -1,34 +1,54 @@
 print("Welcome to WordListGenerator!\n")
 
 try:
+    #List Containing operation codes
+    operations = []
+
+    #List Containing valid operations
+    valid_operations = [1, 2, 3]
+
     # Menu Driven Code
-    choice = int(input("1. Append\n2. Prepend\nEnter Your Choice: "))
+    loop_flag = 'y'
+
+    while (loop_flag == 'y' or loop_flag == 'Y'):
+        choice = int(input("1. Append\n2. Prepend\n3. UpperCase\nEnter Your Choice: "))
+        operations.append(choice)
+        loop_flag = input("Do you want to add more operations?(Y/N): ")
     
-    # If Input choice is invalid
-    if choice not in [1, 2]:
-        print("Invalid Choice! Exiting......")
-        exit(0)
-    
+    # Checking if operations entered by user are valid
+    for i in operations:
+        if i not in valid_operations:
+            print("Invalid Option! Exiting.....")
+            exit()
+
     #Takes in keyword and start-end bounds
     keyword = input("Enter The Keyword: ")
     start = int(input("Enter The Start Number: "))
     end = int(input("Enter The End Number: "))
  
-    # Runs if user wants to append
-    if choice == 1:
-        f = open(f'{keyword}.txt', 'a')
-        for i in range(start, end+1):
-            f.write(f"{keyword}{str(i)}\n")
-        f.close()
-        print(f'{keyword}.txt Created!')
-    
-    # Runs if user wants to prepend
-    else:
-        f = open(f'{keyword}.txt', 'a')
-        for i in range(start, end+1):
-            f.write(f"{str(i)}{keyword}\n")
-        f.close()
-        print(f'{keyword}.txt Created!')
+    seedkeywords = []
+    seedkeywords.append(keyword)
+
+    if 3 in operations:
+        seedkeywords.append(keyword.upper())
+
+    # Runs if user wants to append Numbers
+    if 1 in operations:
+        for word in seedkeywords:
+            f = open(f'{keyword}.txt', 'a')
+            for i in range(start, end+1):
+                f.write(f"{word}{str(i)}\n")
+            f.close()
+            print(f'{keyword}.txt Created!')
+        
+    # Runs if user wants to prepend Numbers
+    if 2 in operations:
+        for word in seedkeywords:
+            f = open(f'{keyword}.txt', 'a')
+            for i in range(start, end+1):
+                f.write(f"{str(i)}{word}\n")
+            f.close()
+            print(f'{keyword}.txt Created!')
 
 except:
     print("Invalid Input! Exiting.....")
